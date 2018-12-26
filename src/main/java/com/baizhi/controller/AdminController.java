3f414a3ca66b6df2ab96f003b4aaeb3b33930962
wjn <471828034@qq.com>
2018-12-26 20:27:07 +0800

    测试更è更新曹曹组

--- src/main/java/com/baizhi/controller/AdminController.java
+++ src/main/java/com/baizhi/controller/AdminController.java
@@ -19,6 +19,7 @@ public class AdminController {
        Admin wjn = adminService.login(username, password);
        if (wjn!=null){
            session.setAttribute("admin",wjn);
+           System.out.println(username);
            return "redirect:/emp/findAll";
        }
        else return "login";
